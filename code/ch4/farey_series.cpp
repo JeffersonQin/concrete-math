b5ed6b7bc05191e2e5cdba64d001b6bb378332c0
gyro永不抽风 <1247006353@qq.com>
2022-03-15 14:43:35 +0800

    feat: add farey series

---
+++ code/ch4/farey_series.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#define fuck cout << __LINE__ << endl
+
+using namespace std;
+
+struct element {
+	int numerator;
+	int denominator;
+};
+
+struct node {
+	struct node *next;
+	struct node *last;
+	struct element val;
+};
+
+void print_farey(struct node *start) {
+	for (struct node *iter = start; iter != nullptr; iter = iter->next)
+		cout << "[" << iter->val.numerator << "/" << iter->val.denominator << "] ";
+	cout << endl;
+}
+
+int main() {
+	int n; cin >> n;
+
+	struct node *start = new node;
+	struct node *end = new node;
+	start->last = nullptr;
+	start->next = end;
+	start->val = (element) {0, 1};
+	end->last = start;
+	end->next = nullptr;
+	end->val = (element) {1, 1};
+
+	cout << "F1" << endl;
+	print_farey(start);
+
+	for (int i = 2; i <= n; i ++) {
+		for (struct node *iter = start; iter != nullptr; iter = iter->next) {
+			if (iter->last == nullptr) continue;
+			struct node *last = iter->last;
+			if (iter->val.denominator + last->val.denominator <= i) {
+				struct node *insert = new node;
+				insert->val = (element) {
+					iter->val.numerator + last->val.numerator,
+					iter->val.denominator + last->val.denominator
+				};
+				insert->last = last;
+				insert->next = iter;
+				last->next = insert;
+				iter->last = insert;
+			}
+		}
+		cout << "F" << i << endl;
+		print_farey(start);
+	}
+	return 0;
+}
